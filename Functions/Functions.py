92c98c803b87bf2475f1cf162934362aba93c140
Tyron Joe <tyronjoe55@gmail.com>
2020-03-18 12:23:50 +0200

    Updated with Task 1

--- Functions/Functions.py
+++
@@ -1,5 +0,0 @@
-def addition(x, y):
-    ans = x + y
-    return ans
-result = addition(5, 7)
-print(result)
---
+++ Functions/functions.py
@@ -0,0 +1,13 @@
+def addition(x, y):
+    ans = x + y
+    return ans
+result = addition(5, 7)
+print(result)
+
+#Task 1
+nights = int(input("Enter Number Of Night\n"))  # User input number of nights
+def hotel_cost(nights):
+    return 140*nights
+
+result = hotel_cost(nights)
+print(result)
